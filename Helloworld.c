adcd3ab926abe85838ce1455498911012fb0e711
tallurutejaswi <74002750+tallurutejaswi@users.noreply.github.com>
2021-05-17 11:17:29 +0530

    helloworld using functions

---
+++ Helloworld.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+// function to print Hello World!
+void printMessage(void)
+{
+    printf("Hello World!");
+}
+ 
+int main()
+{
+     
+    //calling function
+    printMessage();
+ 
+    return 0;
+}
